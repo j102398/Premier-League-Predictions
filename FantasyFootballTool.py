23535c11d45bf848ca3f8ef04912adec8ba0a1de
j102398 <147440159+j102398@users.noreply.github.com>
2023-10-27 18:42:50 +0100

    Update FantasyFootballTool.py

--- FantasyFootballTool.py
+++ FantasyFootballTool.py
@@ -4,12 +4,12 @@ import requests
 
 root = tk.Tk()
 root.title("Premier League Stats")
-root.geometry("500x500")
+root.geometry("500x900")
 
 
-def back_to_main_menu_button():
+def back_to_main_menu_button(row,column):
   back_button = tk.Button(root,text = "Back to main menu", padx=10,pady=10,command = lambda:main_menu(),fg = "white",bg = "black")
-  back_button.grid(row = 10, column = 1,)
+  back_button.grid(row = row, column = column,)
 
 
 
@@ -63,7 +63,7 @@ def display_eplteams():
       if column % 4 == 0:
           row += 1
           column = 0
-  back_to_main_menu_button()
+  back_to_main_menu_button(10,3)
 
 def display_functions(t):
     for widget in root.winfo_children():
@@ -83,7 +83,7 @@ def display_functions(t):
     cleansheets_button = tk.Button(root,text = "Clean sheets",padx=20,pady=20,command = lambda: clean_sheets(t))
     cleansheets_button.grid(row=4,column=0)
 
-    back_to_main_menu_button()
+    back_to_main_menu_button(10,3)
     back_button = tk.Button(root,text = "Back",padx = 5,pady=5,command = lambda: display_eplteams(),fg = "white",bg = "black")
     back_button.grid(row=10,column=0)
 
@@ -107,7 +107,7 @@ def display_top_scorer(t):
     else:
         print("Elements with 'data-stat' 'top_team_scorers' or 'team' were not found on the page.")
 
-    back_to_main_menu_button()
+    back_to_main_menu_button(10,3)
     back_button = tk.Button(root,text = "Back",padx = 5,pady=5,command = lambda: display_functions(t),fg = "white",bg = "black")
     back_button.grid(row=10,column=0)
 
@@ -142,10 +142,136 @@ def display_goals_conceded(t):
   else:
     print("Not found")
 
-  back_to_main_menu_button()
+  back_to_main_menu_button(10,3)
   back_button = tk.Button(root,text = "Back",padx = 5,pady=5,command = lambda: display_functions(t),fg = "white",bg = "black")
   back_button.grid(row=10,column=0)
 
 
+
+
+
+
+
+
+
+
+
+
+def team_total_goals(t):
+  for widget in root.winfo_children():
+    widget.destroy()
+  pageToScrape = requests.get('https://fbref.com/en/comps/9/Premier-League-Stats')
+  soup = BeautifulSoup(pageToScrape.text, "html.parser")
+
+  team_total_goals_elements = soup.find_all(attrs={"data-stat": "goals"})
+  team_elements = soup.find_all(attrs={"data-stat": "team"})
+
+  if team_total_goals_elements and team_elements:
+    for team_total_goals, team in zip(team_total_goals_elements, team_elements):
+      player_data = team_total_goals.get_text(strip=True)
+      team_name = team.get_text(strip=True)
+      if team_name == t:
+        goals = tk.Label(root,text = str(player_data) + " goals" , padx=5, pady=5)
+        goals.grid(row=0,column=0)
+  else:
+    print("Not found")
+
+  team_total_xg_elements = soup.find_all(attrs={"data-stat":"xg"})
+  if team_total_xg_elements and team_elements:
+    for team_total_xg, team in zip(team_total_xg_elements, team_elements):
+      player_data = team_total_xg.get_text(strip=True)
+      team_name = team.get_text(strip=True)
+      if team_name == t:
+        outputtedsentence = "From an expected " + str(player_data) + " goals"
+        goals = tk.Label(root,text = outputtedsentence , padx=5, pady=5)
+        goals.grid(row=1,column=0)
+  back_to_main_menu_button(10,3)
+  back_button = tk.Button(root,text = "Back",padx = 5,pady=5,command = lambda: display_functions(t),fg = "white",bg = "black")
+  back_button.grid(row=10,column=0)
+
+
+
+
+
+def clean_sheets(t):
+  for widget in root.winfo_children():
+    widget.destroy()
+  pageToScrape = requests.get('https://fbref.com/en/comps/9/Premier-League-Stats')
+  soup = BeautifulSoup(pageToScrape.text, "html.parser")
+
+  team_elements = soup.find_all(attrs={"data-stat": "team"})
+  clean_sheets_total = soup.find_all(attrs={"data-stat": "gk_clean_sheets"})
+  clean_sheets_percentage = soup.find_all(attrs={"data-stat":"gk_clean_sheets_pct"})
+
+  if clean_sheets_total and team_elements:
+
+    for clean_sheets, team in zip(clean_sheets_total, team_elements):
+      player_data = clean_sheets.get_text(strip=True)
+      team_name = team.get_text(strip=True)
+      if team_name == t:
+        outputtedsentence =   str(player_data) + " total clean sheets"
+        clean_sheetslabel = tk.Label(root,text = outputtedsentence , padx=0,pady=0)
+        clean_sheetslabel.grid(row=1 , column = 0)
+
+  if clean_sheets_percentage and team_elements:
+    for clean_sheets_pctg, team in zip(clean_sheets_percentage, team_elements):
+      player_data = clean_sheets_pctg.get_text(strip=True)
+      team_name = team.get_text(strip=True)
+      if team_name == t:
+        outputtedsentence = "(" + str(player_data) + "% clean sheet percentage)"
+        cleansheetpercentagelabel = tk.Label(root,text = outputtedsentence , padx=5, pady=5)
+        cleansheetpercentagelabel.grid(row=2,column=0)
+  back_to_main_menu_button(10,3)
+  back_button = tk.Button(root,text = "Back",padx = 5,pady=5,command = lambda: display_functions(t),fg = "white",bg = "black")
+  back_button.grid(row=10,column=0)
+
+
+def display_epltable():
+  for widget in root.winfo_children():
+    widget.destroy()
+  pageToScrape = requests.get('https://fbref.com/en/comps/9/Premier-League-Stats')
+  soup = BeautifulSoup(pageToScrape.text, "html.parser")
+  team_elements = soup.find_all(attrs={"data-stat": "team"})
+  position_elements = soup.find_all(attrs={"data-stat": "rank"})
+  points_elements = soup.find_all(attrs={"data-stat":"points"})
+  matches_played_elements = soup.find_all(attrs={"data-stat":"games"})
+  if team_elements and position_elements and points_elements and matches_played_elements:
+    row = 0
+
+
+    for position, team, points,matchesPlayed in zip(position_elements, team_elements,points_elements,matches_played_elements):
+
+        position = position.get_text(strip=True)
+        team_name = team.get_text(strip=True)
+        points = points.get_text(strip=True)
+        matchesPlayed = matchesPlayed.get_text(strip=True)
+
+
+        outputtedsentenceposition = tk.Label(root,text=(str(position)+  ". "  ))
+        outputtedsentenceposition.grid(row=row,column=0,pady=1)
+
+        outputtedsentenceteam = tk.Label(root,text=(str(team_name)))
+        outputtedsentenceteam.grid(row=row,column=2,pady=1)
+
+
+        outputtedsentencepoints = tk.Label(root,text=(str(points) ))
+        outputtedsentencepoints.grid(row=row,column = 4,pady=1)
+
+        outputtedsentencematchesplayed = tk.Label(root,text=str(matchesPlayed))
+        outputtedsentencematchesplayed.grid(row=row,column=6,pady=1)
+
+        row += 1
+        if row == 21:
+
+          back_to_main_menu_button(10,7)
+          break
+
+
+
+
+
+
+main_menu()
+root.mainloop()
 
 
