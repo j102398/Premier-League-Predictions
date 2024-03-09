1656f075401aa122317cfbf8b836eee0dd4927fd
j102398 <147440159+j102398@users.noreply.github.com>
2024-03-09 13:18:30 +0000

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -102,13 +102,15 @@ def insert_data(gameweek):
 
         # Construct the query with parameterized queries
         query = '''
-            INSERT INTO predictions (gameweek, home_team, away_team, home_previous_xg, score, home_previous_away_xg,
+            INSERT INTO predictions (gameweek, home_team, away_team, home_previous_xg, score, home_previous_xg,
                                      home_points, home_last_5_points, home_goal_diff, home_progressive_carries,
                                      home_progressive_passes, home_xg, away_previous_xg, away_previous_home_xg,
                                      away_points, away_last_5_points, away_goal_diff, away_progressive_carries,
                                      away_progressive_passes, away_xg)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         '''
+
+
 
         # Execute the query with parameters
         prediction_cursor.execute(query, (gameweek, home_team, away_team, previous_home_xg, previous_score,
@@ -122,9 +124,95 @@ def insert_data(gameweek):
     prediction_connection.commit()
 
 
+def delete_comparisons_table():
+    prediction_cursor.execute("DELETE FROM comparisons")
+    prediction_connection.commit()
 
+
+
+
+
+def create_comparisons_table():
+    query = sql_create_table = '''
+CREATE TABLE IF NOT EXISTS comparisons (
+    fixture TEXT,
+    more_previous_xg REAL,
+    score INTEGER,
+    more_points INTEGER,
+    more_last_5_points INTEGER,
+    more_goal_diff INTEGER,
+    more_progressive_carries INTEGER,
+    more_progressive_passes INTEGER,
+    more_xg REAL
+);
+'''
+    prediction_cursor.execute(query)
+
+
+def insert_comparisons():
+    # Get the data for home teams
+    prediction_cursor.execute(
+        'SELECT home_team, home_points, home_last_5_points, home_goal_diff, home_progressive_carries, home_progressive_passes, home_xg, away_previous_xg, score FROM predictions')
+    home_teams_data = prediction_cursor.fetchall()
+
+    # Get data for away teams
+    prediction_cursor.execute(
+        'SELECT away_team, away_points, away_last_5_points, away_goal_diff, away_progressive_carries, away_progressive_passes, away_xg, home_previous_xg, score FROM predictions')
+    away_teams_data = prediction_cursor.fetchall()
+
+    # Insert the comparisons into the comparisons table
+    for x in range(0, len(home_teams_data)):
+        # Get variables
+        home_team = home_teams_data[x][0]
+        away_team = away_teams_data[x][0]
+        home_points = home_teams_data[x][1]
+        away_points = away_teams_data[x][1]
+        home_last_5_points = home_teams_data[x][2]
+        away_last_5_points = away_teams_data[x][2]
+        home_goal_diff = home_teams_data[x][3]
+        away_goal_diff = away_teams_data[x][3]
+        home_progressive_carries = home_teams_data[x][4]
+        away_progressive_carries = away_teams_data[x][4]
+        home_progressive_passes = home_teams_data[x][5]
+        away_progressive_passes = away_teams_data[x][5]
+        home_xg = home_teams_data[x][6]
+        away_xg = away_teams_data[x][6]
+        away_previous_xg = home_teams_data[x][7]
+        home_previous_xg = away_teams_data[x][7]
+        score = home_teams_data[x][8]
+        previous_home_goals = int(score[0])
+        previous_away_goals = int(score[2])
+
+
+
+        # Make comparisons
+        # Make comparisons
+        more_previous_xg = round(away_previous_xg - home_previous_xg, 2)
+        more_points = round(home_points - away_points)
+        more_goal_diff = round(home_goal_diff - away_goal_diff)
+        more_progressive_carries = round(home_progressive_carries - away_progressive_carries)
+        more_progressive_passes = round(home_progressive_passes - away_progressive_passes)
+        more_xg = round(home_xg - away_xg, 2)
+        more_last_5_points = home_last_5_points - away_last_5_points
+        more_score = previous_away_goals - previous_home_goals
+
+        # Construct the query
+        query = '''
+            INSERT INTO comparisons (fixture, more_previous_xg, more_points, more_goal_diff, more_progressive_carries, more_progressive_passes, more_xg, more_last_5_points, score)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        '''
+
+        # Execute the query with parameters
+        prediction_cursor.execute(query, (
+            home_team + " vs " + away_team, more_previous_xg, more_points, more_goal_diff, more_progressive_carries,
+            more_progressive_passes, more_xg, more_last_5_points, more_score))
+
+    prediction_connection.commit()
 
 clear_prediction_table()
 create_prediction_table()
-insert_data(28)
 
+delete_comparisons_table()
+create_comparisons_table()
+insert_data(28)
+insert_comparisons()
